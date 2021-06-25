81818ff5b03871f7dabaa218f613557ecede5f9c
Daniel <danieldawit33@gmail.com>
2021-06-24 20:23:01 -0400

    Add inventory setup

--- src/main/kotlin/me/nanigans/infiniteblocks/InfiniteBlocks.kt
+++ src/main/kotlin/me/nanigans/infiniteblocks/InfiniteBlocks.kt
@@ -1,10 +1,46 @@
 package me.nanigans.infiniteblocks
 
+import me.nanigans.infiniteblocks.commands.Shop
+import me.nanigans.infiniteblocks.gui.Button
+import me.nanigans.infiniteblocks.gui.Page
+import me.nanigans.infiniteblocks.gui.ShopGUI
+import org.bukkit.Material
+import org.bukkit.inventory.ItemStack
+import org.bukkit.plugin.Plugin
 import org.bukkit.plugin.java.JavaPlugin
+import org.bukkit.plugin.java.JavaPlugin.getPlugin
 
 class InfiniteBlocks(): JavaPlugin() {
 
+    val shopGUI: ShopGUI = this.setupShopInventory();
+
     override fun onEnable() {
+
+        this.getCommand("shop")?.setExecutor(Shop());
+
+    }
+
+    fun setupShopInventory(): ShopGUI{
+
+        val pages: MutableList<Page> = mutableListOf();
+        var page: Page = Page("Shop", 54);
+        var i: Int = 0;
+        for (material in Material.values()) {
+            if(material.isBlock){
+
+                if(i < 54){
+                    page.addButton(i, Button(ItemStack(material), { item: ItemStack -> println(item) }, true));
+                }else{
+                    i = -1;
+                    pages.add(page);
+                    page = Page("Shop", 54);
+                }
+
+                i++;
+            }
+        }
+
+        return ShopGUI(pages.toTypedArray(), this);
 
     }
 
--- src/main/kotlin/me/nanigans/infiniteblocks/commands/Shop.kt
+++ src/main/kotlin/me/nanigans/infiniteblocks/commands/Shop.kt
@@ -1,12 +1,22 @@
 package me.nanigans.infiniteblocks.commands
 
+import org.bukkit.ChatColor
 import org.bukkit.command.Command
 import org.bukkit.command.CommandExecutor
 import org.bukkit.command.CommandSender
+import org.bukkit.entity.Player
 
 class Shop(): CommandExecutor {
-    override fun onCommand(p0: CommandSender, p1: Command, p2: String, p3: Array<out String>): Boolean {
-        TODO("Not yet implemented")
+    override fun onCommand(sender: CommandSender, command: Command, aliase: String, args: Array<out String>): Boolean {
+
+        if(sender is Player && command.name.equals("shop", ignoreCase = true)){
+
+
+
+        }else{
+            sender.sendMessage("${ChatColor.RED} You are not a player");
+        }
+        return true;
 
     }
 }
